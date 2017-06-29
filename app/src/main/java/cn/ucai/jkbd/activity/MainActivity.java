fe8cba8b4345acb8c48450afbf6aeee50df5f381
14wangyuchun <593142940@qq.com>
2017-06-29 23:42:53 +0800

    获取数据

--- app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
@@ -3,9 +3,12 @@ package cn.ucai.jkbd.activity;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
 import cn.ucai.jkbd.R;
+import cn.ucai.jkbd.bean.item;
+import cn.ucai.jkbd.utils.OkHttpUtils;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -15,6 +18,23 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
     }
     public  void text(View view){
+
+        OkHttpUtils<item> utils=new OkHttpUtils<>(getApplicationContext());
+        String uri="http://101.251.196.90:8080/JztkServer/examInfo ";
+        utils.url(uri)
+                .targetClass(item.class)
+                .execute(new OkHttpUtils.OnCompleteListener<item>(){
+                    @Override
+                    public  void onSuccess(item result){
+                        Log.e("main","result="+result);
+                    }
+                    @Override
+                    public void onError(String error){
+                        Log.e("main","error="+error);
+                    }
+                });
+
+
         startActivity(new Intent(MainActivity.this,Andomactivity.class));
     }
     public  void exit(View view){
