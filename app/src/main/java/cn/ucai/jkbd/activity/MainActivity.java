6e2416198ba04378d24febbee885d9e601d5e646
14wangyuchun <wangyu14>
2017-06-29 16:10:08 +0800

    添加随机测试点击事件，和更改项目图标

--- app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
+++ app/src/main/java/cn/ucai/jkbd/activity/MainActivity.java
@@ -1,7 +1,9 @@
 package cn.ucai.jkbd.activity;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import cn.ucai.jkbd.R;
 
@@ -10,6 +12,12 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.random_test);
+        setContentView(R.layout.activity_main);
+    }
+    public  void text(View view){
+        startActivity(new Intent(MainActivity.this,Andomactivity.class));
+    }
+    public  void exit(View view){
+       finish();
     }
 }
