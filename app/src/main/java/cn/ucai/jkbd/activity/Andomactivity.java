62bb1c5a6bd683402da099e9142cc71cdb66ae6e
14wangyuchun <wangyu14>
2017-06-29 11:22:27 +0800

    半成品考试试题界面

---
+++ app/src/main/java/cn/ucai/jkbd/activity/Andomactivity.java
@@ -0,0 +1,20 @@
+package cn.ucai.jkbd.activity;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import cn.ucai.jkbd.R;
+
+/**
+ * Created by Administrator on 2017/6/29.
+ */
+
+public class Andomactivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.random_test);
+    }
+}
